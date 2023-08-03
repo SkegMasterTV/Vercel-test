b7e623183f1e5b343a2160bf45c73d9cea2a9256
SkegMasterTV <skegmastertv@gmail.com>
2023-08-03 16:46:11 +0200

    add connecttoMongoDB

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-// Add Express
+// Add Express et Mongoose
 const express = require("express");
+const mongoose = require('mongoose');
+const Detection = require('./models/detection');
+const uri = "mongodb+srv://Skeg:rZIxBT2aGJ5lOQiM@go-fullstack.9nnj4kf.mongodb.net/?retryWrites=true&w=majority";
+const uri2 = "mongodb+srv://vercel:4QgX1J34vLaU3xik@go-fullstack.9nnj4kf.mongodb.net/?retryWrites=true&w=majority";
 
 // Initialize Express
 const app = express();
+
+mongoose.connect(uri,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // Create GET request
 app.get("/", (req, res) => {
