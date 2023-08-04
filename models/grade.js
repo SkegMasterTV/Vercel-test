e377717842581114aa2950cda27c4549cfa7ae66
SkegMasterTV <skegmastertv@gmail.com>
2023-08-04 22:02:20 +0200

    revert

--- models/grade.js
+++ models/grade.js
@@ -4,6 +4,6 @@ const gradeSchema = mongoose.Schema({
 student_id: { type: Number, required: true },
   scores: { type: Array, required: false },
   class_id: {type: Number, required: true}
-}, {collection : 'sample_training'}); 
+}, {collection : 'test'}); 
 
 module.exports = mongoose.model('Grade', gradeSchema);
