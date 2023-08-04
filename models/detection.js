1eff99372b31c2a3803cf490a1d4e49aa4eb7d1f
SkegMasterTV <skegmastertv@gmail.com>
2023-08-04 23:07:06 +0200

    timestamps add

--- models/detection.js
+++ models/detection.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const detectionSchema = mongoose.Schema({
   nfcreaderid: { type: String, required: true },
-  //description: { type: String, required: true },
+  description: { type: Array, required: true },// voir mongoose schema
   //imageUrl: { type: String, required: true },
  // userId: { type: String, required: true },
   //price: { type: Number, required: true },
-});
+},{ timestamps: true });
 
 module.exports = mongoose.model('Detection', detectionSchema);
