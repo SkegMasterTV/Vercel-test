d72ab75b850b4e3641da4b822906de2ca5ccd9cf
SkegMasterTV <skegmastertv@gmail.com>
2023-08-05 15:55:42 +0200

    update detection

--- models/detection.js
+++ models/detection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const detectionSchema = mongoose.Schema({
   nfcReaderId: { type: Number, required: true },
-  nfcTagId: { type: Number, required: true }
+  nfcTagId: { type: Number, required: true },
   //imageUrl: { type: String, required: true },
   //userId: { type: String, required: true },
   //price: { type: Number, required: true },
